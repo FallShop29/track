91c3bfc939b98bee1fa58267c81d6e125f578018
FallShop29 <naufalazra9@gmail.com>
2025-09-03 16:29:20 +0700

    Update and rename send.jd to send.js

--- api/send.js
+++ api/send.js
@@ -3,8 +3,10 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const BOT_TOKEN = "8427844482:AAHHXCP_psaehlBnm8SHBEEFvhTehYw2gEY";  // ganti
-  const CHAT_ID = "7600526426";      // ganti
+  // ganti dengan data bot kamu
+  const BOT_TOKEN = "8427844482:AAHHXCP_psaehlBnm8SHBEEFvhTehYw2gEY";
+  const CHAT_ID = "7600526426";
+
   const { text } = req.body;
 
   try {
